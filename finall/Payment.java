7f0adf2c5275c9176acec48e212731a37f341eee
Ekya1411 <142556703+Ekya1411@users.noreply.github.com>
2024-09-25 12:55:57 +0700

    add: Mentor, Payment, User

---
+++ finall/Payment.java
@@ -0,0 +1,28 @@
+package finall;
+
+public class Payment {
+    private Course course;
+    private double amount;
+    private String paymentStatus;
+    private Date transactionDate;
+
+    // Constructor
+    public Payment(Course nameCourse,String paymentStatus, double amount, Date transactionDate) {
+        this.paymentStatus = paymentStatus;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.course = nameCourse;
+        
+    }
+
+    // Method untuk menampilkan detail pembayaran
+    public void showPaymentDetails() {
+        System.out.println("\n=== Payment Details ===");
+        System.out.println("Course Name     : " + course.getCourseName());
+        System.out.println("Payment Status  : " + paymentStatus);
+        System.out.println("Amount Paid     : $" + amount);
+        System.out.println("Transaction Date: " + transactionDate.getFormattedDate());
+        System.out.println("===========================");
+    }
+
+}
